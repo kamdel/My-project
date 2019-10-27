af09c87cf71dd450fb11a3cdab6aa07b59a0477a
Kamil Delimat <kamildel@gmail.com>
2019-10-27 12:45:03 +0100

    commit

--- main.js
+++ main.js
@@ -1,35 +1,35 @@
-const spanText = document.querySelector('.text');;
-const spanCursor = document.querySelector('.cursor');
-const txt = ['Witam! :) ', 'To jest moja strona', 'Zapraszam :)'];
+const spanText = document.querySelector(".text");
+const spanCursor = document.querySelector(".cursor");
+const txt = ["Witam! :) ", "To jest moja strona", "Zapraszam :)"];
 
 let actLetter = -20;
 let actText = 0;
 
 // Implementacja
 const dodaj = () => {
+  if (actLetter >= 0) {
+    spanText.textContent += txt[actText][actLetter];
+  }
+  actLetter++;
 
-   if (actLetter >= 0) {
-      spanText.textContent += txt[actText][actLetter];
-   }
-   actLetter++;
-
-   if (actLetter === txt[actText].length) {
-      actText++;
-      if (actText === txt.length) { // return zamiast acttext = 0;
-         actText = 0;
-      }
-      return setTimeout(() => {
-         actLetter = -15;
-         spanText.textContent = '';
-         dodaj();
-      }, 2000)
-   }
-   setTimeout(dodaj, 100)
-}
+  if (actLetter === txt[actText].length) {
+    actText++;
+    if (actText === txt.length) {
+      // return zamiast acttext = 0;
+      actText = 0;
+    }
+    return setTimeout(() => {
+      actLetter = -15;
+      spanText.textContent = "";
+      dodaj();
+    }, 2000);
+  }
+  setTimeout(dodaj, 100);
+};
 dodaj(); //pierwsze wywołanie
 
 // Animacja kursora (zostaw bez zmian)
 const cursorAnimation = () => {
-   spanCursor.classList.toggle('active');
-}
+  spanCursor.classList.toggle("active");
+};
 setInterval(cursorAnimation, 400);
